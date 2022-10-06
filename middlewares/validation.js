1109478f1fd17086d83a8387441c2dd9ea3d17e1
Kirill Dianov <kirill.dianoff@yandex.ru>
2022-10-06 15:17:15 +0300

    backend 2.10

--- middlewares/validation.js
+++ middlewares/validation.js
@@ -45,9 +45,9 @@ const authValidator = celebrate({
 
 const registretionValidator = celebrate({
   body: Joi.object().keys({
-    name: Joi.string().required().min(2).max(30),
-    about: Joi.string().required().min(2).max(30),
-    avatar: Joi.string().required().pattern(urlPattern),
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
+    avatar: Joi.string().pattern(urlPattern),
     email: Joi.string().required().email(),
     password: Joi.string().required(),
   }),
